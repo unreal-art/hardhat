98257f6d8c6cc19064bf637d75abe00f40668bf7
Nasfame <laciferin2024@protonmail.com>
2025-08-01 03:54:30 +0530

    refactor: simplify account configuration by using private keys directly

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -55,34 +55,12 @@ const config: _Config = {
       saveDeployments: true,
       // deploy: "hardhat",
       chainId: 1337,
-      accounts: [
-        {
-          privateKey:
-            process.env.PRIVATE_KEY ||
-            "beb00ab9be22a34a9c940c27d1d6bfe59db9ab9de4930c968b16724907591b3f",
-          balance: `${1000000000000000000000000n}`,
-        },
-        ...PRIVATE_KEYS.map((privateKey) => {
-          return {
-            privateKey: privateKey,
-            balance: `${1000000000000000000000000n}`,
-          }
-        }),
-      ],
-    },
+      accounts: PRIVATE_KEYS,
     etherlink: {
       forking: {
         url: "https://node.mainnet.etherlink.com",
         blockNumber: 22332526,
-        accounts: {
-          ...PRIVATE_KEYS.map((privateKey) => {
-            return {
-              privateKey: privateKey,
-              balance: `${1000000000000000000000000n}`,
-            }
-          }),
-        },
-      },
+        accounts:PRIVATE_KEYS,
       chainId: 42793,
       accounts: PRIVATE_KEYS,
       saveDeployments: true,
